2172a3f34140767fe12c83c313609e0445a19dc7
Medina Magdalena <magdalenamedina25@gmail.com>
2025-10-17 16:20:34 -0300

    se agregaron los Middlewares json y morgan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import app from "./src/app.js";
 
+const PORT = process.env.PORT || 3001;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 const app = express();
+
+//Middlewares
+dotenv.config({ quiet: true });
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (res, req) => {
   req.send("Hello World");
